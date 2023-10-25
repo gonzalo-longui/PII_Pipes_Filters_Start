ee82393166d2cb5a2e117d2f56b976cde12963f6
longuigon <longuigon+git@gmail.com>
2023-10-25 10:54:38 -0300

    ej 3

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using CompAndDel.Pipes;
 using CompAndDel.Filters;
+using Ucu.Poo.Twitter;
 
 namespace CompAndDel
 {
@@ -16,12 +17,15 @@ namespace CompAndDel
             
             PictureProvider provider0 = new PictureProvider();
             IPicture picture = provider0.GetPicture("beer.jpg");
+            var twitterImage = new TwitterImage();
 
             PictureProvider provider1 = new PictureProvider();
             provider1.SavePicture(pipeSerial0.Send(picture), @"beer1.jpg");
+            Console.WriteLine(twitterImage.PublishToTwitter("", @"beer1.jpg"));
 
             PictureProvider provider2 = new PictureProvider();
             provider2.SavePicture(pipeSerial1.Send(picture), @"beer2.jpg");
+            Console.WriteLine(twitterImage.PublishToTwitter("", @"beer2.jpg"));
         }
     }
 }
